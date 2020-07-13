a42abffa6b99ac0a70397506af87421153ff51cc
jason <bettsjason@outlook.com>
2020-07-13 15:09:43 -0500

    working star ratings

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Text, View, useState } from "react";
+import { FaStar } from "react-icons/fa";
+
+
+const createArray = length => [...Array(length)];
+
+
+const Star = ({ selected = false, onSelect = f => f }) => (
+  <FaStar color={selected ? "red" : "grey"} onClick={onSelect} />
+);
+
+
+
+export default function StarRating({ totalStars = 10 }) {
+
+  const [selectedStars, setSelectedStars] = useState(10);
+
+  return (
+    <>
+      {createArray(totalStars).map((n,i) => (
+        <Star 
+         key={i} 
+         selected={ selectedStars > i }
+         onSelect={() => setSelectedStars( i + 1 )} 
+        />
+      ))}
+      <p>
+        {selectedStars} of {totalStars} stars
+      </p>
+    </>
+  );
+}
